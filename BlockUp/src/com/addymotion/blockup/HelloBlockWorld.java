793c5546cfb8ca24f7da223965980b62b883a3ce
MisterAddy <addyfilm@yahoo.com>
2015-02-21 18:20:23 +0000

    Changed outputs to test class

--- BlockUp/src/com/addymotion/blockup/HelloBlockWorld.java
+++ BlockUp/src/com/addymotion/blockup/HelloBlockWorld.java
@@ -3,5 +3,6 @@ package com.addymotion.blockup;
 public class HelloBlockWorld {
 	public static void main(String[] args){
 		System.out.println("A new and exciting project");
+		System.out.println("Testing commiting with Git");
 	}
 }
