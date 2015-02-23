78015b77266668804cb906156bf258029087779a
rfljo2 <rfljo2@cam.ac.uk>
2015-02-23 22:52:35 +0000

    Added a test string to print

--- BlockUp/src/com/addymotion/blockup/HelloBlockWorld.java
+++ BlockUp/src/com/addymotion/blockup/HelloBlockWorld.java
@@ -4,5 +4,7 @@ public class HelloBlockWorld {
 	public static void main(String[] args){
 		System.out.println("A new and exciting project");
 		System.out.println("Testing commiting with Git");
+		System.out.println("New test 2");
+		
 	}
 }
