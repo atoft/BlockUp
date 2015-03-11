6946c9ef792d9e2af52326a41493f6e8eea75680
Tom Read-Cutting <tr395@cam.ac.uk>
2015-03-11 14:23:06 +0000

    Finished actionlistener implementation.

--- BlockUp/src/uk/ac/cam/rfljo2/BlockUp/input/ControlListener.java
+++ BlockUp/src/uk/ac/cam/rfljo2/BlockUp/input/ControlListener.java
@@ -12,13 +12,21 @@ import java.awt.event.ActionListener;
  */
 public abstract class ControlListener implements ActionListener {
 	
-	public static final String ROTATE_BLOCK_LEFT = "r";
-	public static final String ROTATE_BLOCK_RIGHT = "r";
-	public static final String MOVE_BLOCK_LEFT = "r";
-	public static final String MOVE_BLOCK_RIGHT = "r";
-	public static final String ROTATE_BLOCK_180 = "r";
-	public static final String SPEED_UP_BLOCK = "r";
-	public static final String PAUSE_GAME = "p";
+	//for the actionlistener functionality, as long as each string is unique, it doesn't matter what they are defined as.
+	public static final String ROTATE_BLOCK_LEFT = "rbl";
+	public static final String ROTATE_BLOCK_RIGHT = "rbr";
+	public static final String MOVE_BLOCK_LEFT = "mbl";
+	public static final String MOVE_BLOCK_RIGHT = "mbr";
+	public static final String ROTATE_BLOCK_180 = "rb180";
+	public static final String SPEED_UP_BLOCK = "sub";
+	public static final String SLAM_BLOCK = "slb";
+	public static final String PAUSE_GAME = "pgm";
+	public static final String NAVIGATE_UP = "nup";
+	public static final String NAVIGATE_DOWN = "ndo";
+	public static final String NAVIGATE_LEFT = "nle";
+	public static final String NAVIGATE_RIGHT = "nri";
+	public static final String ENTER = "ent";
+	public static final String GO_BACK = "gob";
 	
 	public abstract void rotateBlockLeft();
 	
@@ -51,8 +59,47 @@ public abstract class ControlListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch(e.getActionCommand()) {
+			case ROTATE_BLOCK_LEFT:
+				rotateBlockLeft();
+			break;
+			case ROTATE_BLOCK_RIGHT:
+				rotateBlockRight();
+			break;
+			case MOVE_BLOCK_LEFT:
+				moveBlockLeft();
+			break;
+			case MOVE_BLOCK_RIGHT:
+				moveBlockRight();
+			break;
+			case ROTATE_BLOCK_180:
+				rotateBlock180();
+			break;
+			case SPEED_UP_BLOCK:
+				speedUpBlock();
+			break;
+			case SLAM_BLOCK:
+				slamBlock();
+			break;
 			case PAUSE_GAME:
 				pauseGame();
+			break;
+			case NAVIGATE_UP:
+				navigateUp();
+			break;
+			case NAVIGATE_DOWN:
+				navigateDown();
+			break;
+			case NAVIGATE_LEFT:
+				navigateLeft();
+			break;
+			case NAVIGATE_RIGHT:
+				navigateRight();
+			break;
+			case ENTER:
+				enter();
+			break;
+			case GO_BACK:
+				goBack();
 			break;
 		}
 	}
